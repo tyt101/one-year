fbde6326c8277d71510ece3faabff4fcd3735095
tangytin@163.com <tangytin@163.com>
2023-12-22 11:49:59 +0800

    try to get my custom cli

---
+++ my_custom_cli/test-cli/README.md
@@ -0,0 +1,4 @@
+
+## 原文链接
+
+https://blog.csdn.net/weixin_42855188/article/details/130595059?ops_request_misc=&request_id=&biz_id=102&utm_term=npm%E5%8F%91%E5%B8%83%E4%B8%80%E4%B8%AA%E7%A7%BB%E5%8A%A8%E7%AB%AF%E5%9F%BA%E7%A1%80%E6%A8%A1%E6%9D%BF&utm_medium=distribute.pc_search_result.none-task-blog-2~all~sobaiduweb~default-0-130595059.142^v96^pc_search_result_base5&spm=1018.2226.3001.4187
---
+++ my_custom_cli/test-cli/lib/mirror.js
@@ -0,0 +1,7 @@
+module.exports = {
+  '0': 'direct:https://gitee.com/meme-project/m-template-vue2-mb.git',
+  '1': 'direct:https://gitee.com/meme-project/m-template-vue2-pc.git',
+  '2': 'direct:https://gitee.com/meme-project/m-template-vue3-mb.git',
+  '3': 'direct:https://gitee.com/meme-project/m-template-vue3-pc.git',
+  '4': 'direct:https://gitee.com/meme-project/m-template-wx.git',
+};
