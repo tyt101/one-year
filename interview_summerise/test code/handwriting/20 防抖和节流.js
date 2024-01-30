2d7a90631b92b0749d383fc70d03b099ce908a2a
tangytin@163.com <tangytin@163.com>
2024-01-30 18:06:16 +0800

    update

--- interview_summerise/test code/handwriting/20 防抖和节流.js
+++ interview_summerise/test code/handwriting/20 防抖和节流.js
@@ -1,7 +1,7 @@
 // 防抖：一个事件在ns内频繁的触发不会响应回调函数，只有在事件触发ns后不继续触发事件才会响应回调函数。
-
+// 应用：search搜索联想，屏幕resize
 // 节流：一个事件频繁触发，事件只会每各ns响应一次。
-
+// 应用：点击事件，页面滚动事件，触底加载
 function debounce(fn, wait) {
   let timer = null
   return function (...args) {
